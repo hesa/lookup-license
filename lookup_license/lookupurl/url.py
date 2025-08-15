4b08481ef513baedfb94123e12d2fe3809f0b62f
Henrik Sandklef <hesa@sandklef.com>
2025-08-16 00:14:16 +0200

    fix flake8 issues

--- lookup_license/lookupurl/url.py
+++ lookup_license/lookupurl/url.py
@@ -21,4 +21,17 @@ class Url(LookupURL):
         }
 
         ret = self.lookup_license_urls(url, [[urls]])
+
+        licenses_object = self.gitrepo.licenses([], ret)
+        repositories = []
+
+        ret['provided'] = url
+        ret['meta'] = {}
+        ret['meta']['url_type'] = 'url'
+        ret['meta']['config_details'] = []
+        ret['meta']['repository'] = ', '.join(repositories)
+        ret['details']['config_licenses'] = licenses_object['config_license']
+        ret['identified_license'] = licenses_object['identified_license']
+        ret['identified_license_string'] = licenses_object['identified_license_string']
+
         return ret
