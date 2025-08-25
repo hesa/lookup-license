e3e23df03ec864e82de81fd6e6d0bd897c281c15
Henrik Sandklef <hesa@sandklef.com>
2025-08-25 19:08:16 +0200

    rewrite init so calling methods do not have to init

--- lookup_license/cache.py
+++ lookup_license/cache.py
@@ -1,41 +1,63 @@
+# SPDX-FileCopyrightText: 2025 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from pathlib import Path
 from diskcache import Cache
 
+import logging
+
 class LookupLicenseCache():
 
-    def __init__(self):
-        self.cache = None
+    def _init_cache(self, update=False):
+        logging.debug('LookupLicenseCache _init_cache')
+        self.cache = Cache(f'{Path.home()}/.ll/')
+        self.enabled = True
+        self.update_mode = True
 
-    def _init_cache(self):
-        if not self.cache:
-            self.cache = Cache(f'{Path.home()}/.ll/')
+    def set_update_mode(self, enable_update=True):
+        self.update_mode = enable_update
+
+    def disable(self):
+        self.enabled = False
+
+    def enable(self):
+        self.enabled = True
 
     def add(self, key, value):
-        self._init_cache()
-        self.cache.add(key, value)
-    
+        if not self.enabled:
+            logging.debug("LookupLicenseCache is disabled, will not store ")
+        logging.debug(f'LookupLicenseCache add {key}')
+
+        if not self.cache.add(key, value):
+            if self.update_mode:
+                self.cache.set(key, value)
+                logging.debug('LookupLicenseCache updated: {key}')
+        else:
+            logging.debug('LookupLicenseCache added: {key}')
+            logging.debug('LookupLicenseCache Objects in cache: {self.cache.size}')
+
+    def get(self, key):
+        logging.debug(f'LookupLicenseCache get {key}')
+        if not self.enabled:
+            raise Exception("LookupLicenseCache is disabled")
+        if self.update_mode:
+            raise Exception("LookupLicenseCache update mode enabled")
+
+        return self.cache[key]
+
     def close(self):
-        self._init_cache()
         self.cache.close()
-    
+
+    def clear(self):
+        self.cache.clear()
+
     def __new__(cls):
         if not hasattr(cls, 'llcache'):
+            logging.debug('Creating LookupLicenseCache object')
             cls.llcache = super(LookupLicenseCache, cls).__new__(cls)
+            cls.llcache._init_cache()
         return cls.llcache
 
-    def list(self):
-        self._init_cache()
-        return list(self.cache)
-
-
-cache = LookupLicenseCache()
-cache = LookupLicenseCache()
-cache = LookupLicenseCache()
-cache = LookupLicenseCache()
-cache = LookupLicenseCache()
-
-cache.add("hesa", "ksksk")
-
-print(str(cache.list()))
-
-cache.close()
+    def cache(self):
+        return self.cache
