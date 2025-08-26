3f85f6537e97f957aa22b24d51d6baf1d3d47d24
Henrik Sandklef <hesa@sandklef.com>
2025-08-26 23:02:33 +0200

    add support for purl/coordinates variables in lookup

---
+++ lookup_license/lookupurl/clearlydefined.py
@@ -0,0 +1,85 @@
+from purltools import purl2clearlydefined
+from lookup_license.lookupurl.lookupurl import LookupURL
+from lookup_license.lookupurl.license_provider import LicenseProvider
+from lookup_license.retrieve import Retriever
+
+import re
+import requests
+import json
+import logging
+import sys
+
+LICENSE_EXPRESSION_PATH = 'licensed.facets.core.discovered.expressions'
+
+class ClearlyDefined(LicenseProvider):
+
+    def name(self):
+        return 'https://clearlydefined.io/'
+    
+    def purl_to_coordinate(self, purl):
+        return purl2clearlydefined(purl)
+        
+    def purl_to_coordinate_url(self, purl):
+        coord = purl2clearlydefined(purl)
+        coord_url = f'https://api.clearlydefined.io/definitions/{coord}'
+        return coord_url
+        
+    def lookup_license_package_impl(self, orig_url, pkg_type, pkg_namespace, pkg_name, pkg_version, pkg_qualifiers=None, pkg_subpath=None):
+        #if pkg_type = 'pkg'
+        print("package_impl :) :)")
+        coordinates = f'{pkg_type}/{pkg_namespace}/-/{pkg_name}/{pkg_version}'
+        coord_url = f'https://api.clearlydefined.io/definitions/{coordinates}'
+        print("coord_url: " + str(coord_url))
+        return self.lookup_license_impl(coord_url)
+        
+        
+    def lookup_license_impl(self, url):
+        retriever = Retriever()
+        error_msg = None
+        print("HERE I AM ")
+        if url.startswith('pkg:'):
+            # Try url as a purl
+            try:
+                coord_url = self.purl_to_coordinate_url(url)
+            except:
+                logging.debug(f'Could not convert {url} to a coordinate')
+                coord_url = None
+        else:
+            coord_url = url
+
+        if coord_url:
+            try:
+                retrieved_result = retriever.download_url(coord_url)
+                success = retrieved_result['success']
+            except:
+                success = False
+                error_msg = f'Coult not download {coord_url}.'
+        else:
+            success = False
+            error_msg = f'Coult not convert {url} to a coordinate.'
+        if not success:
+            identified_license = None
+            coord_url = None
+        else:
+            decoded_content = retrieved_result['decoded_content']
+            json_data = json.loads(decoded_content)
+        
+            try:
+                inner_json = json_data
+                for key in LICENSE_EXPRESSION_PATH.split('.'):
+                    inner_json = inner_json.get(key)
+                inner_json.sort()
+                identified_license = ' AND '.join(inner_json)
+            except:
+                logging.debug(f'Failed getting data from clearlydefined, with "{key}" out of {LICENSE_EXPRESSION_PATH}')
+                error_msg = f'Failed getting data from clearlydefined, with "{key}" out of {LICENSE_EXPRESSION_PATH}'
+                identified_license = None
+                coord_url = None
+
+        ret = {
+            'license': identified_license,
+            'data_url': coord_url,
+            'data_path': LICENSE_EXPRESSION_PATH,
+            'error_message': error_msg
+        }
+        return ret
