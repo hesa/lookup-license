96320636baceb840d00433f5b5303b35991a2cb6
Henrik Sandklef <hesa@sandklef.com>
2025-09-01 23:32:36 +0200

    fix flake8 warnings

--- lookup_license/lookupurl/clearlydefined.py
+++ lookup_license/lookupurl/clearlydefined.py
@@ -1,13 +1,9 @@
-from purltools import purl2clearlydefined
-from lookup_license.lookupurl.lookupurl import LookupURL
+from purltools import purl2clearlydefined  # noqa: I900
 from lookup_license.lookupurl.license_provider import LicenseProvider
 from lookup_license.retrieve import Retriever
 
-import re
-import requests
 import json
 import logging
-import sys
 
 LICENSE_EXPRESSION_PATH = 'licensed.facets.core.discovered.expressions'
 
@@ -15,15 +11,15 @@ class ClearlyDefined(LicenseProvider):
 
     def name(self):
         return 'https://clearlydefined.io/'
-    
+
     def purl_to_coordinate(self, purl):
         return purl2clearlydefined(purl)
-        
+
     def purl_to_coordinate_url(self, purl):
         coord = purl2clearlydefined(purl)
         coord_url = f'https://api.clearlydefined.io/definitions/{coord}'
         return coord_url
-        
+
     def lookup_license_package_impl(self, orig_url, pkg_type, pkg_namespace, pkg_name, pkg_version, pkg_qualifiers=None, pkg_subpath=None):
         if pkg_version:
             pkg_version_str = f'/{pkg_version}'
@@ -32,8 +28,7 @@ class ClearlyDefined(LicenseProvider):
         coordinates = f'{pkg_type}/{pkg_namespace}/-/{pkg_name}{pkg_version_str}'
         coord_url = f'https://api.clearlydefined.io/definitions/{coordinates}'
         return self.lookup_license_impl(coord_url)
-        
-        
+
     def lookup_license_impl(self, url):
         retriever = Retriever()
         error_msg = None
@@ -41,7 +36,7 @@ class ClearlyDefined(LicenseProvider):
             # Try url as a purl
             try:
                 coord_url = self.purl_to_coordinate_url(url)
-            except:
+            except Exception:
                 logging.debug(f'Could not convert {url} to a coordinate')
                 coord_url = None
         else:
@@ -51,7 +46,7 @@ class ClearlyDefined(LicenseProvider):
             try:
                 retrieved_result = retriever.download_url(coord_url)
                 success = retrieved_result['success']
-            except:
+            except Exception:
                 success = False
                 error_msg = f'Coult not download {coord_url}.'
         else:
@@ -63,23 +58,22 @@ class ClearlyDefined(LicenseProvider):
         else:
             decoded_content = retrieved_result['decoded_content']
             json_data = json.loads(decoded_content)
-        
+
             try:
                 inner_json = json_data
                 for key in LICENSE_EXPRESSION_PATH.split('.'):
                     inner_json = inner_json.get(key)
                 inner_json.sort()
                 identified_license = ' AND '.join(inner_json)
-            except:
+            except Exception:
                 logging.debug(f'Failed getting data from clearlydefined, with "{key}" out of {LICENSE_EXPRESSION_PATH}')
                 error_msg = f'Failed getting data from clearlydefined, with "{key}" out of {LICENSE_EXPRESSION_PATH}'
                 identified_license = None
-                #coord_url = json_data
 
         ret = {
             'license': identified_license,
             'data_url': coord_url,
             'data_path': LICENSE_EXPRESSION_PATH,
-            'error_message': error_msg
+            'error_message': error_msg,
         }
         return ret
