fa4c6ea4dc7ba69ce63570fc3fba1bfc7d54f8ef
Henrik Sandklef <hesa@sandklef.com>
2025-09-02 22:12:49 +0200

    add method parameters_to_coordinate_url

--- lookup_license/lookupurl/license_provider.py
+++ lookup_license/lookupurl/license_provider.py
@@ -2,12 +2,15 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import logging
+
 class LicenseProvider():
 
     def name(self):
         return None
 
     def lookup_license(self, url):
+        logging.debug(f'lookup_license_package {url}')
         data = self.lookup_license_impl(url)
         data['provider'] = self.name()
         data['url'] = url
@@ -16,8 +19,12 @@ class LicenseProvider():
         return data
 
     def lookup_license_package(self, orig_url, pkg_type, pkg_namespace, pkg_name, pkg_version, pkg_qualifiers=None, pkg_subpath=None):
+        logging.debug(f'lookup_license_package {orig_url}, {pkg_type}, {pkg_namespace}, {pkg_name}, {pkg_version}, {pkg_qualifiers}, {pkg_subpath}')
         data = self.lookup_license_package_impl(orig_url, pkg_type, pkg_namespace, pkg_name, pkg_version, pkg_qualifiers=None, pkg_subpath=None)
         data['provider'] = self.name()
         data['url'] = orig_url
         data['status'] = data['license'] is not None
         return data
+
+    def parameters_to_coordinate_url(self, pkg_type, pkg_namespace, pkg_name, pkg_version, pkg_qualifiers=None, pkg_subpath=None):
+        raise Exception('Subclasses to LicenseProvider must implment: parameters_to_coordinate_url')
