1b3f09fecbc8290eae3095e5433fa99aed83848e
Henrik Sandklef <hesa@sandklef.com>
2025-09-04 21:56:47 +0200

    add error handling, lookup from url instead of type

--- lookup_license/lookupurl/factory.py
+++ lookup_license/lookupurl/factory.py
@@ -5,9 +5,12 @@
 from lookup_license.lookupurl.gitrepo import GitRepo
 from lookup_license.lookupurl.gem import Gem
 from lookup_license.lookupurl.purl import Purl
+from lookup_license.lookupurl.purl import Ecosystem
 from lookup_license.lookupurl.url import Url
 from lookup_license.lookupurl.pypi import Pypi
 from lookup_license.lookupurl.swift import Swift
+
+import logging
 
 def _contains(url, strings):
     res = any(map(url.__contains__, strings))
@@ -17,14 +20,39 @@ class LookupURLFactory:
 
     @staticmethod
     def lookupurl(url_type):
+        logging.debug(f'LookupURLFactory:lookup "{url_type}"')
         _lookup_map = {
             'purl': Purl,
-            'pypi': Pypi,
-            'gitrepo': GitRepo,
+            Ecosystem.PYPI.value: Pypi,
+            Ecosystem.PYPI: Pypi,
+            Ecosystem.GITREPO.value: GitRepo,
+            Ecosystem.GITREPO: GitRepo,
             'url': Url,
-            'swift': Swift,
-            'gem': Gem,
+            Ecosystem.SWIFT.value: Swift,
+            Ecosystem.SWIFT: Swift,
+            Ecosystem.GEM: Gem,
+            Ecosystem.GEM.value: Gem,
         }
-        lookup_obj = _lookup_map[url_type]()
+        try:
+            lookup_class = _lookup_map[url_type]
+            lookup_object = lookup_class()
+            return lookup_object
+        except Exception as e:
+            raise Exception(f'Ecosystem "{url_type}" not supported. Exception {e}')
 
-        return lookup_obj
+    @staticmethod
+    def lookupurl_url(url):
+        if not url.startswith('pkg:'):
+            raise Exception(f'Purl "{url}" not a valid purl.')
+        url_type = None
+        if _contains(url, ['gem', 'rubygems']):
+            url_type = Ecosystem.GEM
+        elif _contains(url, ['pypi']):
+            url_type = Ecosystem.PYPI
+        elif _contains(url, ['swift']):
+            url_type = Ecosystem.SWIFT
+
+        if not url_type:
+            raise Exception(f'Purl "{url}" not supported.')
+
+        return LookupURLFactory.lookupurl(url_type)
