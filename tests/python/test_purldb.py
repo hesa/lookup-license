4d446213af60b28f8d6d81ce93b3fff8c144da1b
Henrik Sandklef <hesa@sandklef.com>
2025-09-06 20:25:30 +0200

    add maven test, add comments

--- tests/python/test_purldb.py
+++ tests/python/test_purldb.py
@@ -11,10 +11,17 @@ from lookup_license.lookupurl.purldb import PurlDB
 
 pdb = PurlDB()
 
+# pypi
 def test_parameters_pypi():
     purldb_url = pdb.parameters_to_url('pypi', 'pypi', 'boto3', '1.35.99')
     assert 'https://public.purldb.io/api/collect/?purl=pkg:pypi/pypi/boto3@1.35.99' == purldb_url
 
+# gem
 def test_parameters_gem():
     purldb_url = pdb.parameters_to_url('gem', 'rubygems', 'google-apis-core', '0.11.1')
+    assert 'https://public.purldb.io/api/collect/?purl=pkg:gem/rubygems/google-apis-core@0.11.1' == purldb_url
+
+# maven
+def test_parameters_maven():
+    purldb_url = pdb.parameters_to_url('maven', 'mavencentral', 'google-apis-core', '0.11.1')
     assert 'https://public.purldb.io/api/collect/?purl=pkg:gem/rubygems/google-apis-core@0.11.1' == purldb_url
