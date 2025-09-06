80a3d8b6148c5c3bb409e007479a9c397c73e58c
Henrik Sandklef <hesa@sandklef.com>
2025-09-06 20:25:16 +0200

    add comments

--- tests/python/test_clearlydefined.py
+++ tests/python/test_clearlydefined.py
@@ -11,14 +11,17 @@ from lookup_license.lookupurl.clearlydefined import ClearlyDefined
 
 cd = ClearlyDefined()
 
+# pypi
 def test_parameters_pypi():
     coord_url = cd.parameters_to_url('pypi/pypi', None, 'boto3', '1.35.99')
     assert 'https://api.clearlydefined.io/definitions/pypi/pypi/-/boto3/1.35.99' == coord_url
 
+# gem
 def test_parameters_gem():
     coord_url = cd.parameters_to_url('gem/rubygems', None, 'google-apis-core', '0.11.1')
     assert 'https://api.clearlydefined.io/definitions/gem/rubygems/-/google-apis-core/0.11.1' == coord_url
 
+# maven
 def test_parameters_maven():
     coord_url = cd.parameters_to_url('maven', 'mavencentral', 'org.apache.httpcomponents/httpcore', '4.3')
     assert 'https://api.clearlydefined.io/definitions/maven/mavencentral/org.apache.httpcomponents/httpcore/4.3' == coord_url
