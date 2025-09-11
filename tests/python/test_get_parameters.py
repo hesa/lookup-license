4aa4f1f6447c834e0567725cfc2c7ee5a38c9ae6
Henrik Sandklef <hesa@sandklef.com>
2025-09-11 23:22:32 +0200

    add tests for gem

---
+++ tests/python/test_get_parameters.py
@@ -0,0 +1,72 @@
+#!/bin/env python3
+
+# SPDX-FileCopyrightText: 2025 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import json
+import pytest
+
+from lookup_license.lookupurl.pypi import Pypi
+from lookup_license.lookupurl.gem import Gem
+from lookup_license.lookupurl.clearlydefined import ClearlyDefined
+
+cd = ClearlyDefined()
+
+#
+# pypi
+#
+def test_get_parameters_pypi_package():
+    # from pypi package to parameters
+    pp = Pypi()
+    params = pp.get_parameters('boto3@1.35.99', None)
+    assert 'pypi' == params.get('namespace')
+    assert 'boto3' == params.get('name')
+    assert '1.35.99' == params.get('version')
+
+def test_get_parameters_pypi_package_no_version():
+    # from pypi package to parameters
+    pp = Pypi()
+    params = pp.get_parameters('boto3', '1.35.99')
+    assert 'pypi' == params.get('namespace')
+    assert 'boto3' == params.get('name')
+    assert '1.35.99' == params.get('version')
+
+def test_get_parameters_pypi_purl():
+    # from purl package to parameters
+    pp = Pypi()
+    params = pp.get_parameters('pkg:pypi/pypi/boto3@1.35.99', None)
+    assert 'pypi' == params.get('namespace')
+    assert 'boto3' == params.get('name')
+    assert '1.35.99' == params.get('version')
+
+    
+#
+# Gem
+#
+def test_get_parameters_gem_package():
+    # from gem package to parameters
+    g = Gem()
+    params = g.get_parameters('google-apis-core@0.11.1', None)
+    assert 'gem' == params.get('namespace')
+    assert 'google-apis-core' == params.get('name')
+    assert '0.11.1' == params.get('version')
+
+def test_get_parameters_gem_package_no_version():
+    # from gem package to parameters
+    g = Gem()
+    params = g.get_parameters('google-apis-core', '0.11.1')
+    print("param " + str(params))
+    assert 'gem' == params.get('namespace')
+    assert 'google-apis-core' == params.get('name')
+    assert '0.11.1' == params.get('version')
+
+def test_get_parameters_gem_purl():
+    # from purl package to parameters
+    g = Gem()
+    params = g.get_parameters('pkg:gem/google-apis-core@0.11.1', None)
+    assert 'gem' == params.get('namespace')
+    assert 'google-apis-core' == params.get('name')
+    assert '0.11.1' == params.get('version')
+
+
